84576d9d9731ca9ea193713a519c6c9c20043b34
Mattl02 <hoemac16@htl-kaindorf.ac.at>
2019-03-21 08:31:20 +0100

    Initial Comit

---
+++ src/main/java/htl/hoetzl/firstmavenproject/Anlage.java
@@ -0,0 +1,29 @@
+
+package htl.hoetzl.firstmavenproject;
+
+/**
+ *
+ * @author Matthias
+ */
+public class Anlage {
+    private double anschaffungswert;
+    private int nutzungsdauer;
+    private double buchwert;
+    
+    public Anlage(double anschaffungswert, int nutzungsdauer) {
+        this.anschaffungswert = anschaffungswert;
+        this.nutzungsdauer = nutzungsdauer;
+    }
+    
+    public double jahresAfa(){
+        return anschaffungswert / nutzungsdauer;
+    }
+    
+    public void updateBuchwert(int jahreGenutzt){
+        //this.buchwert = anschaffungswert - (anschaffungswert/nutzungsdauer * jahreGenutzt);
+    }
+
+    public double getBuchwert() {
+        return buchwert;
+    }
+}
